6610d5c0e72531e7d058cb5bb160c58c66e981de
Sanjay Krishnadas <sanjaykrishnadas.online@gmail.com>
2024-11-16 15:00:17 +0530

    added speed insights and analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import localFont from "next/font/local";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -24,6 +26,8 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics/>
+        <SpeedInsights/>
       </body>
     </html>
   );
